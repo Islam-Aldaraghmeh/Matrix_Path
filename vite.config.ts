8bf9ea428f4ce230992bea99c1263b944fd5afd7
Islam Ardaraghmeh <islamaldaraghmeh@gmail.com>
2025-11-03 13:01:10 +0400

    Fix try 1

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,17 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const explicitBase = env.VITE_BASE_PATH || env.BASE_PATH;
   const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1];
-  const resolvedBase =
-    mode === 'production'
-      ? (explicitBase
-          ? normalizeBase(explicitBase)
-          : repoName
-            ? normalizeBase(repoName)
-            : '/')
-      : '/';
+  let resolvedBase = '/';
+
+  if (mode === 'production') {
+    if (explicitBase) {
+      resolvedBase = normalizeBase(explicitBase);
+    } else if (repoName) {
+      resolvedBase = normalizeBase(repoName);
+    } else {
+      resolvedBase = './';
+    }
+  }
 
   return {
     plugins: [react()],
